1ca46dd620e0950951604b3a4ffd8771f1914210
Nikhilrai8-max <rainikhil339@gmail.com>
2026-05-29 00:13:52 +0530

    creating auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    return res.status(200).json({
-        message: "User registered successfully"
-    })
+    // get user details from frontend
+    // validation of user details like whether some details are missing or not
+    // check if user already exists or not using the email id
+    // check if files are there or not and upload the files to cloudinary and get the url of the uploaded files
+    // if file uploaded than upload it to cloudnairy and get the url of the uploaded file and save it in the database
+    // remove password from the user details before sending the response to the frontend
+    
 })
 
 export { registerUser }
